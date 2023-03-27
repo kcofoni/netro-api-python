0eb03591849433af869f4a67ff7650fe2ad95e59
kcofoni <75487677+kcofoni@users.noreply.github.com>
2023-03-27 12:47:31 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,33 +1,12 @@
 from setuptools import setup, find_packages
-from codecs import open
-from os import path
-
-here = path.abspath(path.dirname(__file__))
-
-# Get the long description from the README file
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
-    long_description = f.read()
 
 setup(
-   
     name='netro-api-python',
-    version='0.2',
+    version='blue',
     description='Netro API',
-    long_description=long_description,
-
-    packages=find_packages(exclude=('tests', 'docs')),
-    
-    #The project's main homepage.
+    description='this package contains a Netro API python wrapper',
+    packages=find_packages(exclude=('tests*', 'testing*', 'doc*')),
     url='https://github.com/kcofoni/netro-api-python.git/',
-
-    # Author details
     author='kcofoni',
     author_email='kcofoni@gmail.com',
-
-    license='MIT',
-
-    keywords = ['Netro'],
-    classifiers=[],
-
-
 )
